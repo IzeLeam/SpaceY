0e98a8d22ae3ca12e9e82c816709b4fe8db1330f
Elvin-19 <elvinmaubert@gmail.com>
2024-03-18 18:49:48 +0100

    Compteur Asteroides

--- client/src/components/AsteroidCounter.tsx
+++ client/src/components/AsteroidCounter.tsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import '../styles/AsteroidCounter.css';
+
+function getTimeStampMin() {
+    let date = new Date().getTime();
+    let timeStampMin = Math.floor(date / 100000) - 17000000;
+    return timeStampMin ;
+}
 
 function AsteroidCounter() {
+    let timerID = null;
+    let counter = getTimeStampMin();
+    let nbAsteroids = 600000 + counter ;
+
+    function updateTime() {
+        const timer = document.getElementsByClassName("count")[0];
+        timer.innerHTML = formalizeNumber(nbAsteroids++);
+    }
+
+    function formalizeNumber(n : number) {
+        // Convertir le nombre en une chaîne de caractères
+        let numberString = String(n);
+
+        // Créer un tableau pour stocker les parties de la chaîne
+        let formattedParts = [];
+
+        // Parcourir la chaîne de caractères de droite à gauche
+        for (let i = numberString.length - 1, j = 0; i >= 0; i--, j++) {
+            // Ajouter chaque chiffre à un tableau
+            if (j > 0 && j % 3 === 0) {
+                // Si le nombre de chiffres ajoutés est un multiple de 3, ajouter un espace
+                formattedParts.unshift(" ");
+            }
+            formattedParts.unshift(numberString[i]);
+        }
+
+        // Joindre les parties formatées en une seule chaîne et la retourner
+        return formattedParts.join("");
+    }
+
+    async function timeAsync() {
+        updateTime();
+        await new Promise((resolve) => (timerID = setTimeout(resolve, 60000)));
+    }
+
+    /* Start the timer when the page is loaded */
+    window.onload = function() {
+        updateTime()
+        timerID = setInterval(timeAsync, 60000);
+    }
     return (
         <div className="asteroidcounter-container" id="asteroidCount">
-
+            <h2> Number of known astéroïds by humanity : </h2>
+            <h2 className="count"></h2>
         </div>
     )
 }
