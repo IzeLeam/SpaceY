20df9c90d04e7f824ee98ecbf1772804f1ca5b42
IzeLeam <luca.pourceau@gmail.com>
2025-10-16 15:37:25 +0200

    fix

--- client/src/components/Earth.tsx
+++ client/src/components/Earth.tsx
@@ -4,7 +4,7 @@ function Earth() {
     const [img, setImg] = useState('');
 
     useEffect(() => {
-        fetch('https://spacey.luptilu.fr/api/earth/picture')
+        fetch('https://spacey.lucaprc.fr/api/earth/picture')
             .then(response => response.text())
             .then(data => setImg(data));
     }, []);
